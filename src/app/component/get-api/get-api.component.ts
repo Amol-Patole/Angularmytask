18fdd8a8a5eda5c7cb7bec20359e9f998f8db0c2
shritesh aher <shritesh.aher@neebal.com>
2024-09-18 18:03:40 +0530

    done

--- src/app/component/get-api/get-api.component.ts
+++ src/app/component/get-api/get-api.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-get-api',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './get-api.component.html',
   styleUrl: './get-api.component.css'
 })
